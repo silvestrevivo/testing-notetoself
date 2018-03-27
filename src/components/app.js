3ef4680fd9ba5ec50bb9970ccd31b2bb583ed6c8
silvestrevivo <silvestrevivomillan@gmail.com>
2018-03-27 14:00:28 +0200

    general setup with starter and first component

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Form, FormControl, Button } from 'react-bootstrap'
+
+class App extends Component {
+  state = {}
+  render () {
+    return (
+      <div>
+        <h2>Note to Self</h2>
+        <Form>
+          <FormControl />
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default App
