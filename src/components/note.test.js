5ce6959031d1f13eac6eb06138bcbd583e3ead43
silvestrevivo <silvestrevivomillan@gmail.com>
2018-08-27 12:10:44 +0200

    refractoring unmount() function

--- src/components/note.test.js
+++ src/components/note.test.js
@@ -5,25 +5,26 @@ import Note from './note'
 let wrapped
 // we define an global variable to containt element to mount
 
-beforeEach(() => {
-  wrapped = mount(<Note note={{ text: 'test note' }} />)
+describe('Note', () => {
+  /*
+    why { text: 'test note' } => we have to find text as a Jquery selector
+    and this is the way woth Enzyme to define it.
+  */
+  beforeEach(() => {
+    wrapped = mount(<Note note={{ text: 'test note' }} />)
   /*
     Another way to do this is, using destructuring:
 
     const props = { note: { text: 'test note' } }
     wrapped = mount(<Note {...props} />)
   */
-})
+  })
 
-afterEach(() => {
-  wrapped = mount(<Note note={{ text: 'test note' }} />)
-})
-
-describe('Note', () => {
-  /*
-    why { text: 'test note' } => we have to find text as a Jquery selector
-    and this is the way woth Enzyme to define it.
-  */
+  afterEach(() => {
+    wrapped.unmount()
+  // this occurs after each test occurs
+  // recommended when we use mount() methode to avoid problems with the DOM
+  })
 
   it('renders the note text', () => {
     // console.log(note.debug())  => log the component to show it on console
